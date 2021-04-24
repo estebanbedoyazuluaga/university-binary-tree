3b4373e797e4bb3fbc4117231e7f3e683796075a
Esteban Bedoya <ebedoya83134@umanizales.edu.co>
2021-04-23 22:34:08 -0500

    First commit. Application is working fine. All requirements in the assignment are met.

---
+++ BinaryTreeUniversity/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinaryTreeUniversity{
+    class Position{
+        public String Name;
+        public float Salary;
+
+        public Position(String Name, float Salary){
+            this.Name = Name;
+            this.Salary = Salary;
+        }
+    }
+}
---
+++ BinaryTreeUniversity/UniversityTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace BinaryTreeUniversity{
+    class UniversityTree{
+        public PositionNode Root;
+
+        public void CreatePosition(PositionNode parent, 
+                                    Position positionToCreate, 
+                                    string parentPositionName){
+            
+
+            PositionNode newNode = new PositionNode();
+            newNode.Position = positionToCreate;
+            if (Root == null){
+                Root = newNode;
+                return;
+            }
+            if (parent == null){
+                return;
+            }
+            if (parent.Position.Name == parentPositionName){
+                if (parent.left == null){
+                    parent.left = newNode;
+                    return;
+                }
+                parent.right = newNode;
+                return;
+            }
+            CreatePosition(parent.left, positionToCreate, parentPositionName);
+            CreatePosition(parent.right, positionToCreate, parentPositionName);
+        }
+
+        public void PrintTree(PositionNode from){
+            if (from == null)
+            {
+                return;
+            }
+            Console.WriteLine($"{from.Position.Name} : {from.Position.Salary}");
+            PrintTree(from.left);
+            PrintTree(from.right);
+        }
+
+        public float AddSalaries(PositionNode from){
+            if(from == null){
+                return 0;
+            }
+            return from.Position.Salary + AddSalaries(from.left) + AddSalaries(from.right);
+        }
+    }
+}
